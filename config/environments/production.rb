e9dc033a60a8371cd8402da6d1a218cb652c8f16
zuozuo <c_yzuo@groupon.com>
2015-07-06 01:33:16 +0800

    asset config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,8 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_mailer.asset_host = "http://123.57.254.33"
+  config.action_controller.asset_host = config.action_mailer.asset_host 
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
